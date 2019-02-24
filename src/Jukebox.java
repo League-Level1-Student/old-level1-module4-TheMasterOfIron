aa184972856cdad529c5a4e3e529638003f3ff4d
blackbeltcam <patriciatishler@gmail.com>
2019-02-24 11:30:33 -0800

    Jukebox
    Calming waves are a work in progress.

--- src/Jukebox.java
+++ src/Jukebox.java
@@ -1,9 +1,10 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2018
  *    Level 1
  */
-
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -11,7 +12,10 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import javazoom.jl.player.advanced.AdvancedPlayer;
@@ -19,13 +23,20 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 /* 1. Download the JavaZoom jar from here: http://bit.ly/javazoom
  * 2. Right click your project and add it as an External JAR (Under Java Build Path > Libraries).*/
 
-public class Jukebox implements Runnable {
-
+public class Jukebox implements Runnable, ActionListener {
+	JButton seagullbutton = new JButton();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	Song s1 = new Song("Seagull sounds (1).mp3");
+JButton rickrollbutton = new JButton();
+Song s2 = new Song("Rick Astley - Never Gonna Give You Up (Video).mp3");
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Jukebox());
+
 	}
 
-           public void run() {
+	public void run() {
 
 		// 3. Find an mp3 on your computer or on the Internet.
 		// 4. Create a Song
@@ -33,18 +44,42 @@ public class Jukebox implements Runnable {
 		// 5. Play the Song
 
 		/*
-		 * 6. Create a user interface for your Jukebox so that the user can to
-		 * choose which song to play. You can use can use a different button for
-		 * each song, or a picture of the album cover. When the button or album
-		 * cover is clicked, stop the currently playing song, and play the one
-		 * that was selected.
+		 * 6. Create a user interface for your Jukebox so that the user can to choose
+		 * which song to play. You can use can use a different button for each song, or
+		 * a picture of the album cover. When the button or album cover is clicked, stop
+		 * the currently playing song, and play the one that was selected.
 		 */
-          }
+
+		seagullbutton.setText("Seagull Noises");
+		frame.add(panel);
+		panel.add(seagullbutton);
+		frame.setVisible(true);
+		seagullbutton.addActionListener(this);
+		rickrollbutton.setText("Calming Waves");
+		rickrollbutton.addActionListener(this);
+		panel.add(rickrollbutton);
+frame.pack();
+
+	}
+
 	/* Use this method to add album covers to your Panel. */
 	private JLabel loadImage(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
 		Icon icon = new ImageIcon(imageURL);
 		return new JLabel(icon);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		if (buttonPressed == (seagullbutton)) {
+			s1.play();
+		}
+		if (buttonPressed == (rickrollbutton)) {
+			s2.play();
+		}
+
 	}
 
 }
@@ -57,8 +92,7 @@ class Song {
 	private InputStream songStream;
 
 	/**
-	 * Songs can be constructed from files on your computer or Internet
-	 * addresses.
+	 * Songs can be constructed from files on your computer or Internet addresses.
 	 * 
 	 * Examples: <code> 
 	 * 		new Song("everywhere.mp3"); 	//from default package 
@@ -133,4 +167,3 @@ class Song {
 		}
 	}
 }
-
