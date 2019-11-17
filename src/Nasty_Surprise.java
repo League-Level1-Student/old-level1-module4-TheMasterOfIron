e92c7374e832b8e7cd85db68ae4c6f7578c63b73
ShadowWolf <owenmorris16@gmail.com>
2019-11-17 10:51:46 -0800

    Nasty Surprise
    Done.

--- src/Nasty_Surprise.java
+++ src/Nasty_Surprise.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -7,31 +9,51 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-public class Nasty_Surprise {
-	private void showPictureFromTheInternet(String imageUrl) {
-	    try {
-	        URL url = new URL("https://www.google.com/search?q=puppy&sxsrf=ACYBGNT6qMpD_RCYnnDe4w0_TQQgo35Stg:1573414087512&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjI-JSesODlAhUZIjQIHS3ZDugQ_AUIEigB&biw=934&bih=720&dpr=1.25#imgrc=NuHW-1lFpv8eoM:");
-	        Icon icon = new ImageIcon(url);
-	        JLabel imageLabel = new JLabel(icon);
-	        JFrame frame = new JFrame();
-	        frame.add(imageLabel);
-	        frame.setVisible(true);
-	        frame.pack();
-	    } catch (MalformedURLException e) {
-	        e.printStackTrace();
-	    }
-	}
-	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
+import javax.swing.SwingUtilities;
+
+public class Nasty_Surprise implements Runnable, ActionListener {
 	JButton trick = new JButton();
 	JButton treat = new JButton();
-	frame.setVisible(true);
-	frame.add(panel);
-	panel.add(trick);
-	panel.add(treat);
-	trick.setText("Trick");
-	treat.setText("Treat");
-	frame.pack();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+
+	private void showPictureFromTheInternet(String imageUrl) {
+		try {
+			URL url = new URL(imageUrl);
+			Icon icon = new ImageIcon(url);
+			JLabel imageLabel = new JLabel(icon);
+			JFrame frame = new JFrame();
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Nasty_Surprise());
+	}
+
+	public void run() {
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(trick);
+		panel.add(treat);
+		trick.setText("Trick");
+		treat.setText("Treat");
+		trick.addActionListener(this);
+		treat.addActionListener(this);
+		frame.pack();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		JButton buttonPressed = (JButton) e.getSource();
+		if (buttonPressed == (trick)) {
+			showPictureFromTheInternet("https://cdn.mos.cms.futurecdn.net/BwL2586BtvBPywasXXtzwA-320-80.jpeg");
+		}
+		if (buttonPressed == (treat)) {
+			showPictureFromTheInternet("https://pbs.twimg.com/profile_images/778230226761375744/X3_-Whev_400x400.jpg");
+		}
 	}
 }
