4accb91b60ee6c71fa0583b64cf82a05f8edcea4
ShadowWolf <owenmorris16@gmail.com>
2020-02-02 11:25:38 -0800

    Ainray Amegay
    ainray amegayainray amegayainray amegayainray amegayainray amegayainray amegayainray amegayainray amegayainray amegayainray amegayainray amegayainray amegayainray amegayainray amegay

---
+++ src/extra/Pig_Latin_Translator_Runner.java
@@ -0,0 +1,46 @@
+package extra;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class Pig_Latin_Translator_Runner implements Runnable, ActionListener {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JTextField texter = new JTextField(15);
+JTextField dexter = new JTextField(15);
+JButton translateButton = new JButton();
+PigLatinTranslator pigLatinTranslator = new PigLatinTranslator();
+
+public static void main(String[] args) {
+	SwingUtilities.invokeLater(new Pig_Latin_Translator_Runner());
+
+}
+public void run() {
+	
+	frame.add(panel);
+	panel.add(texter);
+	panel.add(translateButton);
+	panel.add(dexter);
+	translateButton.addActionListener(this);
+	translateButton.setText("Translate");
+	frame.show();
+	frame.pack();
+	
+
+}
+
+public void actionPerformed(ActionEvent e) {
+	JButton buttonPressed = (JButton) e.getSource();
+	String textToTranslate = texter.getText();
+	if (buttonPressed == (translateButton)) {
+		System.out.println("translating " + textToTranslate + "...");
+				}
+}
+}
