14c04b7cb877b8b73c4fe0dd7c89663a9c7c4294
ShadowWolf <owenmorris16@gmail.com>
2020-11-29 11:27:38 -0800

    I slept in :(
    ;-;

--- src/extra/Practice_Test_1.java
+++ src/extra/Practice_Test_1.java
@@ -3,11 +3,13 @@ package extra;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Practice_Test_1 {
 public static void main(String[] args) {
 	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	JButton hint = new JButton();
 	JButton submit = new JButton();
